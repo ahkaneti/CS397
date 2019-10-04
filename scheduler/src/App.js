ddc9122e41b98828125609350b5b731dd89b4180
ahkaneti <aaronkaneti2020@u.northwestern.edu>
2019-10-03 19:42:23 -0500

    Slice 5

--- scheduler/src/App.js
+++ scheduler/src/App.js
@@ -54,10 +54,45 @@ const Course = ({ course }) => (
   </button>
 );
 
-const CourseList = ({ courses }) => (
-  <div className="buttons">
-    {courses.map(course => <Course key={course.id} course={course} />)}
-  </div>
+const CourseList = ({ courses }) => {
+  const [term, setTerm] = useState('Fall');
+  const termCourses = courses.filter(course => term === getCourseTerm(course));
+  return (
+    <React.Fragment>
+      <TermSelector state={{ term, setTerm }} />
+      <Button.Group>
+        {termCourses.map(course
+          => <Course key={course.id} course={course} state={{ term, setTerm }} />)}
+      </Button.Group>
+    </React.Fragment>
+  );
+};
+const buttonColor = selected => (
+  selected ? 'success' : null
+);
+
+const TermSelector = ({ state }) => (
+  <Button.Group hasAddons>
+    {Object.values(terms)
+      .map(value =>
+        <Button key={value}
+          color={buttonColor(value === state.term)}
+          onClick={() => state.setTerm(value)}
+        >
+          {value}
+        </Button>
+      )
+    }
+  </Button.Group>
+);
+
+const TermSelector = () => (
+  <Button.Group hasAddons>
+    {Object.values(terms)
+      .map(value => <Button key={value}>{value}</Button>
+      )
+    }
+  </Button.Group>
 );
 
 const App = () => {
